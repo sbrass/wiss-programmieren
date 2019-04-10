3f341bae5b0af908dfa4844c78d973dd428f599d
Simon Brass <brass@physik.uni-siegen.de>
2019-04-10 13:50:03 +0000

    Add Blatt 2.

--- 02/aufgabe4.f90
+++ 02/aufgabe4.f90
@@ -1,10 +1,11 @@
 program main
   use rng
+  use iso_fortran_env, only: i64 => INT64, r64 => REAL64
 
   implicit none
 
-  integer :: ri, rj
-  integer :: a1, a2, a3
+  integer(i64) :: ri, rj
+  integer(i64) :: a1, a2, a3
 
   integer :: i
 
@@ -15,8 +16,8 @@ program main
   a3 = 5
 
   do i = 1, 1000
-     ri = xorshift (rj, a1, a2, a3)
-     rj = xorshift (ri, a1, a2, a3)
-     print *, ri / real (2**30), rj  / real (2**30)
+     ri = xorshift64 (rj, a1, a2, a3)
+     rj = xorshift64 (ri, a1, a2, a3)
+     print *, ri / real (2_i64**62, r64), rj  / real (2_i64**62, r64)
   end do
 end program main
--- 02/rng.f90
+++ 02/rng.f90
@@ -1,9 +1,10 @@
 module rng
+  use iso_fortran_env, only: i64 => INT64
   implicit none
 
   private
 
-  public :: lcg, lcg01, xorshift
+  public :: lcg, lcg01, xorshift, xorshift64
 
 contains
   integer function lcg (state, a, c, m) result (r)
@@ -32,4 +33,15 @@ contains
     r = ieor (r, ishft (r, a3))
     r = modulo (r, 2**30)
   end function xorshift
+
+  integer(i64) function xorshift64 (state, a1, a2, a3) result (r)
+    integer(i64), intent(in) :: state
+    integer(i64), intent(in) :: a1
+    integer(i64), intent(in) :: a2
+    integer(i64), intent(in) :: a3
+    r = ieor (state, ishft (state, a1))
+    r = ieor (r, ishft (r, -a2))
+    r = ieor (r, ishft (r, a3))
+    r = modulo (r, 2_i64**62)
+  end function xorshift64
 end module rng
